f9943a896982c5e24166cd1903e917163f25a32f
Willyapee <williampurnamadjaja2424@gmail.com>
2026-02-16 21:56:09 +0700

    week03: (task 1) implement Weapon testing in main

---
+++ src/oop_00000115707_williamasabha/week03/MainWeapon.kt
@@ -0,0 +1,9 @@
+package oop_00000115707_williamasabha.week03
+
+fun main() {
+    val wep = Weapon("Excalibur")
+    wep.dmg = -50
+    wep.dmg = 1000
+    println("Tier = ${wep.tier}, damage dealt = ${wep.dmg}")
+
+}
