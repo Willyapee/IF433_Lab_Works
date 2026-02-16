a11522ee076812d634900999b488fb8f5060c951
Willyapee <williampurnamadjaja2424@gmail.com>
2026-02-16 21:24:46 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000115707_williamasabha/week03/Employee.kt
+++ src/oop_00000115707_williamasabha/week03/Employee.kt
@@ -2,4 +2,9 @@ package oop_00000115707_williamasabha.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+    set(value) {
+        println("Mencoba set gaji ke: $value")
+
+        this.salary = value
+    }
 }
---
+++ src/oop_00000115707_williamasabha/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_00000115707_williamasabha.week03
+
+fun main(){
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
