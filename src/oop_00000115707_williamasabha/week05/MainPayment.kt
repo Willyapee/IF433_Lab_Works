9a005986e8056f9c9d96dc9942856d6f38ca549c
Willyapee <williampurnamadjaja2424@gmail.com>
2026-03-02 16:55:59 +0700

    week05: (task 2) implement payment polymorphism

---
+++ src/oop_00000115707_williamasabha/week05/MainPayment.kt
@@ -0,0 +1,12 @@
+package oop_00000115707_williamasabha.week05
+
+fun main() {
+    val eWallet = EWallet("Orang",50000.0)
+    val creditCard = CreditCard("Orang", 100000.0)
+
+    val methods: List<PaymentMethod> = listOf(eWallet, creditCard)
+
+    for (method in methods) {
+        method.processPayment(75000.0)
+    }
+}
