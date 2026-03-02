b361730cd1eb6009056f8c42c7e722de23c1b41e
Willyapee <williampurnamadjaja2424@gmail.com>
2026-03-02 16:29:27 +0700

    week05: (task 1) test MathHelper overloading in main

---
+++ src/oop_00000115707_williamasabha/week05/MainMathHelper.kt
@@ -0,0 +1,8 @@
+package oop_00000115707_williamasabha.week05
+
+fun main() {
+    val MathHelper = MathHelper()
+    println("Luas Sisi 5 = ${MathHelper.hitungLuas(5)}")
+    println("Luas Panjang 2 dan Lebar 6 = ${MathHelper.hitungLuas(2, 6)}")
+    println("Luas Jari Jari 2.5 = ${MathHelper.hitungLuas(2.5)}")
+}
