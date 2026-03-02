b772b14c891a26feec8b1cf811ca466d68a45405
Willyapee <williampurnamadjaja2424@gmail.com>
2026-03-02 16:40:18 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_00000115707_williamasabha/week05/EWallet.kt
@@ -0,0 +1,16 @@
+package oop_00000115707_williamasabha.week05;
+
+class EWallet(accountName: String, var balance: Double): PaymentMethod(accountName) {
+    override fun processPayment(amount: Double){
+        if(balance >= amount){
+            balance -= amount
+            println("Sukses")
+        } else{
+            println("Saldo tidak cukup")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+    }
+}
