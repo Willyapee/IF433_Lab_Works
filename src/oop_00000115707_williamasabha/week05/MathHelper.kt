34cba2ce1c8caa001f06ce4332a25c461ae66e3f
Willyapee <williampurnamadjaja2424@gmail.com>
2026-03-02 16:24:54 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_00000115707_williamasabha/week05/MathHelper.kt
@@ -0,0 +1,7 @@
+package oop_00000115707_williamasabha.week05
+
+class MathHelper {
+    fun hitungLuas(sisi: Int): Int = sisi * sisi
+    fun hitungLuas(panjang: Int, lebar: Int): Int = panjang * lebar
+    fun hitungLuas(jarijari: Double): Double = 3.14 * jarijari * jarijari
+}
