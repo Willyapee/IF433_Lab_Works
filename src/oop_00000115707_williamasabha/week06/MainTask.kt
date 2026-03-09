ba54136dca0acd930e12f986d68ec808a89ea74b
Willyapee <williampurnamadjaja2424@gmail.com>
2026-03-09 21:20:17 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_00000115707_williamasabha/week06/MainTask.kt
@@ -0,0 +1,8 @@
+package oop_00000115707_williamasabha.week06
+
+fun main() {
+    val myLamp = SmartLamp("L01", "Ruang Tamu")
+    val mySpeaker = SmartSpeaker("S02", "Google Nest Dapur")
+    val myCCTV = SmartCCTV("C03", "Ezviz Garasi")
+
+}
