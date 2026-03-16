ab07508612f9670ef14227a036f1facfa9b977a7
Willyapee <williampurnamadjaja2424@gmail.com>
2026-03-16 21:47:18 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_00000115707_williamasabha/week07/MainGame.kt
+++ src/oop_00000115707_williamasabha/week07/MainGame.kt
@@ -3,4 +3,10 @@ package oop_00000115707_williamasabha.week07
 fun main(){
     GameManager.startGame()
     GameManager.startGame()
+
+    println("Legendary item drop chance: ${ItemRarity.LEGENDARY.dropChance}")
+    val wep1 = Weapon.forgeStarterSword()
+    println("Senjata Awal: ${wep1.item.name}")
+
+
 }
