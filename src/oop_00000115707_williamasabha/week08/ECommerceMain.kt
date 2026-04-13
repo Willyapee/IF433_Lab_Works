f189946e7cf495ce93457fa410adb5d0592c2c7c
Willyapee <williampurnamadjaja2424@gmail.com>
2026-04-13 23:10:45 +0700

    week08: (task) process mixed raw JSON list using as?

---
+++ src/oop_00000115707_williamasabha/week08/ECommerceMain.kt
@@ -0,0 +1,24 @@
+package oop_00000115707_williamasabha.week08
+
+fun main () {
+    val rawApiData: List<Map<String, Any?>> = listOf(
+        mapOf("id" to "E01", "name" to "Laptop", "type" to "ELECTRONIC", "warranty" to 24),
+        mapOf("id" to "C01", "name" to "T-Shirt", "type" to "CLOTHING", "size" to "XL"),
+        mapOf("id" to "E02", "name" to "Mouse", "type" to "ELECTRONIC", "warranty" to "Not An Integer"), // Corrupted warranty
+        mapOf("name" to "Ghost Item", "type" to "CLOTHING"), // Missing ID!
+        mapOf("id" to "X01", "name" to "Unknown", "type" to "FOOD") // Unknown type
+    )
+    val parser = ApiParser()
+
+    for (raw in rawApiData) {
+        try {
+            val product = parser.parseProduct(raw)
+
+            product?.let {
+                parser.checkout(it)
+            } ?: println("Unknown Product Type")
+        } catch (e: IllegalArgumentException) {
+            println("DATA CORRUPTED: ${e.message}")
+        }
+    }
+}
