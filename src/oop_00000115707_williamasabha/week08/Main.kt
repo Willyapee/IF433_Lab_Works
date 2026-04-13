3dc55d058b36ddbae5d9d246a6d50739e26af618
Lnyx90 <lnyx90prive@gmail.com>
2026-04-13 16:24:27 +0700

    week08: implement java interop using !!

--- src/oop_00000115707_williamasabha/week08/Main.kt
+++ src/oop_00000115707_williamasabha/week08/Main.kt
@@ -54,4 +54,8 @@ fun main() {
         println(e.message)
     }
 
+    println("\n=== TEST JAVA INTEROP ===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
+    val statusLength = javaResponse!!.length
+    println("Status dari Java: $javaResponse (Length: $statusLength)")
 }
