2aa7de27af7ae44eeca356449b88b705af9535b5
Willyapee <williampurnamadjaja2424@gmail.com>
2026-04-13 23:08:20 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000115707_williamasabha/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000115707_williamasabha.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productID) {
+        return "TRX-" + productID + "-SUCCESS";
+    }
+}
