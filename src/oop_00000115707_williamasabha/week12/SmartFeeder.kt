a6bbf269dbed6d80068ecbd85b92ff3ee49ce4d0
Willyapee <williampurnamadjaja2424@gmail.com>
2026-05-11 13:17:10 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_00000115707_williamasabha/week12/SmartFeeder.kt
+++ src/oop_00000115707_williamasabha/week12/SmartFeeder.kt
@@ -6,4 +6,7 @@ fun dispenseKibble(
     isJammed: Boolean
 ): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr"}
+    if (isJammed) {
+        throw DispenserJamException()
+    }
 }
